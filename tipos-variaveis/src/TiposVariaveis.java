d9af64f9f73245493848d7d28bcd2ef0bce67aff
VitorFreitas <vitorfreitas581@gmail.com>
2024-07-04 06:34:51 -0300

    cdvdvf

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,25 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+    class Carro {
+
+        String nome;
+        String modelo; 
+        int ano; 
+        int vel; 
+
+
+void acelerar(int acelerar){
+    
+}
+
+    }
+
+
+
+
+
+
+    }
+}
